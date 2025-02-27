24be462d75da9bd48884624040964fd381c9f611
egarrisxn <egarrisxn@gmail.com>
2025-02-27 16:32:57 -0500

    boom!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
+  {
+    ignores: ["**/node_modules/", ".git/", ".vscode/", ".next/", "public/"],
+  },
 ];
 
 export default eslintConfig;
