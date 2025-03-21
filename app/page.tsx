d4fbf536519cbe168b8bf18ae64118aad6b407c7
egarrisxn <egarrisxn@gmail.com>
2025-03-21 17:48:21 -0400

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <section className="flex flex-auto flex-col items-center justify-center px-4 text-center">
-      <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-8xl dark:text-gray-200 sm:dark:border-gray-300/10">
-        XO Starter
+      <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-200 sm:dark:border-gray-300/10">
+        Home
       </h1>
     </section>
   );
